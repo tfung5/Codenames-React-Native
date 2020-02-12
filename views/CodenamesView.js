9b70884f40b84a5d61adf7a83eb36fade915f1fc
Tony Fung <tfung5@gmail.com>
2020-02-11 19:58:53 -0500

    Removed starter links and buttons

--- views/CodenamesView.js
+++ views/CodenamesView.js
@@ -1,8 +1,6 @@
-import * as WebBrowser from "expo-web-browser";
 import React from "react";
 import { StyleSheet, Image, Text, View } from "react-native";
 import Touchable from "react-native-platform-touchable";
-import { Ionicons } from "@expo/vector-icons";
 
 export default class CodenamesView extends React.Component {
   constructor(props) {
@@ -124,72 +122,17 @@ export default class CodenamesView extends React.Component {
       <View>
         <Text style={styles.optionsTitleText}>Red Team</Text>
         {this.renderBoard()}
-        <Touchable
-          style={styles.option}
-          background={Touchable.Ripple("#ccc", false)}
-          onPress={this._handlePressDocs}
-        >
-          <View style={{ flexDirection: "row" }}>
-            <View style={styles.optionIconContainer}></View>
-            <View style={styles.optionTextContainer}>
-              <Text style={styles.optionText}>Read the Expo documentation</Text>
-            </View>
-          </View>
-        </Touchable>
-
-        <Touchable
-          style={styles.option}
-          background={Touchable.Ripple("#ccc", false)}
-          onPress={this._handlePressForums}
-        >
-          <View style={{ flexDirection: "row" }}>
-            <View style={styles.optionIconContainer}>
-              <Ionicons name="ios-chatboxes" size={22} color="#ccc" />
-            </View>
-            <View style={styles.optionTextContainer}>
-              <Text style={styles.optionText}>
-                Ask a question on the Expo forums
-              </Text>
-            </View>
-          </View>
-        </Touchable>
       </View>
     );
   }
-
-  _handlePressDocs = () => {
-    WebBrowser.openBrowserAsync("http://docs.expo.io");
-  };
-
-  _handlePressForums = () => {
-    WebBrowser.openBrowserAsync("http://forums.expo.io");
-  };
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15
-  },
   optionsTitleText: {
     fontSize: 16,
     marginLeft: 15,
     marginTop: 9,
     marginBottom: 12
-  },
-  optionIconContainer: {
-    marginRight: 9
-  },
-  option: {
-    backgroundColor: "#fdfdfd",
-    paddingHorizontal: 15,
-    paddingVertical: 15,
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    borderBottomColor: "#EDEDED"
-  },
-  optionText: {
-    fontSize: 15,
-    marginTop: 1
   },
   boardRow: {
     flexDirection: "row"
