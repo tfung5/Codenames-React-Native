6f2c861e23a00e5e752845e3290f5c3fb01a616b
stellaama <stellama98@gmail.com>
2020-03-09 15:23:22 -0400

    adding chat UI

--- views/ChatView.js
+++ views/ChatView.js
@@ -1,46 +1,66 @@
 import React from "react";
 import io from "socket.io-client";
-import { StyleSheet, Image, Text, TextInput, View } from "react-native";
+import { StyleSheet, Image, Text, TextInput, View, AsyncStorage } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
 
 export default class ChatView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      chatMessage: "",
-      chatMessages: []
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'Professor Grezes',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+      ],
+      user:{
+        _id: 1,
+        name: 'stella',
+        avatar: 'https://placeimg.com/140/140/any',
+      }
     };
+    this.onReceivedMessage = this.onReceivedMessage.bind(this);
   }
 
   componentDidMount = () => {
     this.socket = io("http://127.0.0.1:3000");
-    this.socket.on("chat message", msg => {
-      this.setState({ chatMessages: [...this.state.chatMessages, msg] });
-    });
+    this.socket.on("chat message", this.onReceivedMessage);
   };
 
-  submitChatMessage = () => {
-    this.socket.emit("chat message", this.state.chatMessage);
-    this.setState({ chatMessage: "" });
-  };
+  /**
+   * When the server sends a message to this,
+   *store it in this component's state.
+   */
+  onReceivedMessage(messages) {
+    this.setState((previousState) => {
+      return {
+        messages: GiftedChat.append(previousState.messages, messages),
+      };
+    });
+ }
+
+ /**
+ * When a message is sent, send the message to the server.
+ */
+  onSend(messages=[]) {
+  this.socket.emit('chat message', messages[0]);
+}
 
   render() {
-    const chatMessages = this.state.chatMessages.map(chatMessage => (
-      <Text style={{ borderWidth: 2, top: 500 }}>{chatMessage}</Text>
-    ));
-
     return (
-      <View style={styles.container}>
-        {chatMessages}
-        <TextInput
-          style={{ height: 40, borderWidth: 2, top: 600 }}
-          autoCorrect={false}
-          value={this.state.chatMessage}
-          onSubmitEditing={() => this.submitChatMessage()}
-          onChangeText={chatMessage => {
-            this.setState({ chatMessage });
-          }}
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          renderUsernameOnMessage={true}
+          showUserAvatar={true}
+          user={this.state.user}
         />
-      </View>
     );
   }
 }
