22cd306aa7ee8654d31da978d62fffa7a63d695d
Tony Fung <tfung5@gmail.com>
2020-03-23 07:20:39 -0400

    Added FETCH_TEAMS and UPDATE_TEAMS constants

--- constants/Actions.js
+++ constants/Actions.js
@@ -1,8 +1,10 @@
 export const CHAT_MESSAGE = "CHAT_MESSAGE";
 export const CHOOSE_CARD = "CHOOSE_CARD";
 export const FETCH_BOARD = "FETCH_BOARD";
+export const FETCH_TEAMS = "FETCH_TEAMS";
 export const GENERATE_BOARD = "GENERATE_BOARD";
 export const JOIN_LOBBY = "JOIN_LOBBY";
 export const JOIN_SLOT = "JOIN_SLOT";
 export const START_GAME = "START_GAME";
 export const UPDATE_BOARD = "UPDATE_BOARD";
+export const UPDATE_TEAMS = "UPDATE_TEAMS";
