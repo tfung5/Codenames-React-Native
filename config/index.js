80c9774f39523979689a93e13520e0462765beb3
Tony Fung <tfung5@gmail.com>
2020-03-23 09:52:08 -0400

    Changed config to use local server

--- config/index.js
+++ config/index.js
@@ -1,3 +1,4 @@
 module.exports = {
-  server: "https://codenames-server-capstone.herokuapp.com/"
+  // server: "https://codenames-server-capstone.herokuapp.com/",
+  server: "http://127.0.0.1:3000"
 };
