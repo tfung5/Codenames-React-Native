0901ef995769f32c9817d772077be5c4905e8609
Tony Fung <tfung5@gmail.com>
2020-04-04 05:36:55 -0400

    Updated device IDs and added commenting

--- run-multiple-simulators.sh
+++ run-multiple-simulators.sh
@@ -1,10 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 # Credit for this script: https://stackoverflow.com/questions/53924934/can-i-run-my-expo-app-on-multiple-ios-simulators-at-once
 
-declare -a simulators=(  "1B88BDDC-1F1A-4895-9E02-E6D9EF0CB2ED" "7F420951-4F1F-421D-AF72-055B4C7E56C5"
-"06BC29B8-5B5C-496A-8FC8-31E029B91B5C" "D6C21895-1046-4BB3-88D7-337D3FC644AA")
-# iPhone 8
-# iPhone 8 Plus
+# The device IDs will differ on every computer and install. You will likely have to find the correct ones in Xcode, or look at what's printed out on the terminal after running this command.
+
+# In the event that your Simulator devices gets deleted or messed up, follow the instructions here to reset them: https://stackoverflow.com/questions/27338042/xcode-simulator-not-coming-up-reinstall-possible/48066998
+
+########################################################
+# The following runs 4 simulators of different devices
+########################################################
+
+declare -a simulators=("4F02ADA3-365E-4546-8130-25F671BCEA0F" "B5D714E3-5EBB-4392-94B9-EFAAA20CCF0D" "A68E897A-2F43-4D90-BEAA-E63618CA3DB5" "6166093F-ABCC-4929-9521-2F4827A8EE33")
+# iPhone X
+# iPhone XR
 # iPhone 11
 # iPhone 11 Pro Max
 
