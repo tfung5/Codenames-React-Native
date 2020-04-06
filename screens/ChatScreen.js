249fad5dd4db8ef4977150cc4cab785d97d2e8df
Tony Fung <tfung5@gmail.com>
2020-04-06 06:50:48 -0400

    fix: Keyboard no longer hides GiftedChat input

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,3 +1,8 @@
+/**
+ * Credit to https://github.com/FaridSafi/react-native-gifted-chat/issues/1272
+ * for help fixing the keyboard hiding GiftedChat input
+ */
+
 import React from "react";
 import {
   StyleSheet,
